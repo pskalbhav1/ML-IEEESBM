ef011760ab2871d02dd255a64334aa2b6ae4a1af
yashmandhania <75852673+yashmandhania@users.noreply.github.com>
2021-02-01 14:34:07 +0530

    Update G.py

--- IEEE ML TASK 2/G.py
+++ IEEE ML TASK 2/G.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 no_of_balls = 30
 x = [random.triangular() for i in range(no_of_balls)]
 y = [random.gauss(1, 1.9) for i in range(no_of_balls)]
-colors = [np.random.randint(1,7) for i in range(no_of_balls)]
+colors = [random.randint(1,7) for i in range(no_of_balls)]
 areas = [math.pi * random.randint(7, 18)**2 for i in range(no_of_balls)]
 plt.figure()
 plt.scatter(x, y, s=areas, c=colors, alpha=0.85)
