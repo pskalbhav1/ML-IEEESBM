aa8883c02f93637dd34968424858fbe77f23ffe2
Kartikeya Tiwari <75495474+kark-netizen@users.noreply.github.com>
2021-07-13 13:20:59 +0530

    task_2(updated)

--- OpenCV/Task/task_2.py
+++ OpenCV/Task/task_2.py
@@ -6,33 +6,41 @@ logo=cv.imread('Task/agent_p_logo.png')
 
 #scale image
 def rescaleFrame(frame, scale=0.3):
-    #set scale to a suitable value
-    #works for images, videos and live videos
     width= int(frame.shape[1]*scale)
-    #frame.shape[1] returns width of frame
     height=int(frame.shape[0]*scale)
-    #frame.shape[0] retruns height of frame
     dimensions=(width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
-    return (cv.resize(frame, dimensions, interpolation=cv.INTER_AREA),dimensions)
+def addLogo(logo, img):
+    rowsl,colsl,channels=logo.shape
+    rowsi,colsi,channels=img.shape
+    logoGray=cv.cvtColor(logo,cv.COLOR_BGR2GRAY)
+    threshold=220
+    threshold,mask=cv.threshold(logoGray, threshold, 255, cv.THRESH_BINARY_INV)
+    maskInv=cv.bitwise_not(mask)
+    roi=img[rowsi-rowsl:rowsi,colsi-colsl:colsi]
+    img_bg=cv.bitwise_and(roi,roi,mask=maskInv)
+    img_fg=cv.bitwise_and(logo,logo,mask=mask)
+    dst=cv.add(img_bg,img_fg)
+    img[rowsi-rowsl:rowsi,colsi-colsl:colsi]=dst
+    return img
 
 #rescale logo
-(logo,dimensions)=rescaleFrame(logo)
-#cv.imshow('Logo size', logo)
-#print(dimensions)
+logo=rescaleFrame(logo)
 
 #video capture
+
 xvid=cv.VideoCapture(0)
 fourcc=cv.VideoWriter_fourcc(*'XVID')
 out=cv.VideoWriter('Task/captured_video.avi', fourcc, 20.0, (640,480))
 
 while True:
     isTrue, frame=xvid.read()
-    #adding logo 
-    frame[0:162,0:109]=logo
-    #adding text
+    frame= addLogo(logo,frame)
+    rowsl,colsl,channels=logo.shape
+    rowsi,colsi,channels=frame.shape
     font=cv.FONT_HERSHEY_COMPLEX
-    cv.putText(frame, 'Agent P Industries', (0,180), font,0.5,(200,0,0), 2, cv.LINE_AA)
+    cv.putText(frame, 'Agent P', (colsi-colsl-20,rowsi-rowsl-20), font,1,(0,0,0), 2, cv.LINE_AA)
     out.write(frame)
     cv.imshow('Captured Video', frame)
 
@@ -43,4 +51,3 @@ xvid.release()
 out.release()
 cv.destroyAllWindows() 
 
-cv.waitKey(0)
